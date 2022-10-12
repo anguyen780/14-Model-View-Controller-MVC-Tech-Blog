386f4762df14af0c5f6bd9d081e2dd20556c9f38
Lux <anguyen780@gmail.com>
2022-10-11 22:19:33 -0500

    finished comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 router.get("/", async (req, res) => {
   try {
@@ -21,5 +22,36 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err)
     }
 });
+
+router.post('/', async (req, res) => {
+    try{
+        const newCom = await Comment.create({
+            ...req.body,
+            user_id: req.session.userId,
+            post_id: req.session.postId
+        })
+        res.json(newCom)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+      if (!commentData) {
+        res.status(404).json({ message: 'Comment could not be deleted' });
+        return;
+      }
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
