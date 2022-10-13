4f5054c29630a01053dbb44d293726053cca33cd
Lux <anguyen780@gmail.com>
2022-10-13 00:06:16 -0500

    added css to new post handlebar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -58,7 +58,7 @@ router.get('/dashboard', withAuth, async (req, res) =>{
     const userData = await User.findByPk(req.session.user_id, {
       include: [
         {
-          model: Post
+          model: Post 
         },
       ],
     });
